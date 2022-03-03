092551db4f08a0400ea8b36b4eb33434a5c1f483
Bryan Louie Martinez <>
2022-03-03 16:26:05 +0800

    Initial commit

---
+++ projects/plugly-common/src/lib/plugly-common.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plugly-common',
+  template: `
+    <p>
+      plugly-common works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PluglyCommonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/plugly-common/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of plugly-common
+ */
+
+export * from './lib/plugly-common.service';
+export * from './lib/plugly-common.component';
+export * from './lib/plugly-common.module';
