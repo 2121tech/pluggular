7d5abe9d469ba9e99c56a42da5beafeb78c9a353
Bryan Louie Martinez <>
2022-03-03 18:32:10 +0800

    Rename

--- projects/plugly-common/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of plugly-common
- */
-
-export * from './lib/plugly-common.service';
-export * from './lib/plugly-common.component';
-export * from './lib/plugly-common.module';
