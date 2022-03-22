be123b7eb7323202ee685f3f75f064896889e8cd
joshua regio <regiojosh1@gmail.com>
2022-03-22 17:12:21 +0800

    Implement fromEvent listener

--- projects/common/src/lib/headernav/headernav.component.ts
+++ projects/common/src/lib/headernav/headernav.component.ts
@@ -3,12 +3,6 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export type TAlignment = 'left' | 'center' | 'right';
 
-export enum EAlignment {
-  LEFT = 'left',
-  CENTER = 'center',
-  RIGHT = 'right',
-}
-
 @Component({
   selector: 'pluggular-headernav',
   templateUrl: './headernav.component.html',
@@ -17,7 +11,7 @@ export enum EAlignment {
 export class PluggularHeadernavComponent implements OnInit {
   menuIcon = faBars;
   alignClass = '';
-  @Input() alignItems: TAlignment = EAlignment.RIGHT;
+  @Input() alignItems: TAlignment = 'right';
   @Output() menuClicked = new EventEmitter<boolean>();
   showSideNav = false;
 
@@ -28,13 +22,13 @@ export class PluggularHeadernavComponent implements OnInit {
   constructItemsAlignmentClass(alignment: TAlignment): string {
     let alignClass = '';
     switch (alignment) {
-      case EAlignment.LEFT:
+      case 'left':
         alignClass = 'justify-start';
         break;
-      case EAlignment.CENTER:
+      case 'center':
         alignClass = 'justify-center';
         break;
-      case EAlignment.RIGHT:
+      case 'right':
         alignClass = 'justify-end';
         break;
       default:
