0e5913c360c273875577460105851bd0d2dc722d
joshua regio <regiojosh1@gmail.com>
2022-03-24 15:08:32 +0800

    Create PluggularTextarea component

--- projects/common/src/lib/textarea/textarea.component.spec.ts
+++ projects/common/src/lib/textarea/textarea.component.spec.ts
@@ -1,24 +1,57 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MockBuilder, MockInstance, MockRender, ngMocks } from 'ng-mocks';
 
 import { PluggularTextareaComponent } from './textarea.component';
 
-describe('TextareaComponent', () => {
-  let component: PluggularTextareaComponent;
-  let fixture: ComponentFixture<PluggularTextareaComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [PluggularTextareaComponent],
-    }).compileComponents();
+@Component({
+  selector: 'pluggular-tested',
+  template: `<form [formGroup]="form">
+    <pluggular-textarea formControlName="firstName"></pluggular-textarea>
+  </form> `,
+})
+class TestedComponent {
+  form = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
   });
+}
+
+describe('PluggularTextareaComponent', () => {
+  MockInstance.scope();
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PluggularTextareaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    return MockBuilder(TestedComponent).mock(PluggularTextareaComponent).keep(ReactiveFormsModule);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('sends the correct value to the mock form component', () => {
+    const writeValue = jasmine.createSpy('writeValue');
+
+    MockInstance(PluggularTextareaComponent, 'writeValue', writeValue);
+
+    const fixture = MockRender(TestedComponent);
+    const component = fixture.point.componentInstance;
+
+    expect(writeValue).toHaveBeenCalledWith('');
+
+    const mockControlEl = ngMocks.find(PluggularTextareaComponent);
+    ngMocks.change(mockControlEl, 'foo');
+
+    expect(component.form.get('firstName')?.value).toBe('foo');
+
+    component.form.get('firstName')?.setValue('bar');
+
+    expect(writeValue).toHaveBeenCalledWith('bar');
+  });
+
+  it('error should be defined if form is submitted without fulfilling form control validation', () => {
+    const fixture = MockRender(TestedComponent);
+    const component = fixture.point.componentInstance;
+
+    const mockControlEl = ngMocks.find(PluggularTextareaComponent);
+
+    ngMocks.touch(mockControlEl);
+
+    expect(component.form.get('firstName')?.errors).toBeTruthy();
   });
 });
