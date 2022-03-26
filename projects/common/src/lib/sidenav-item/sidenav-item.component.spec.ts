249193ceab63df5a25e8fcd665dc5bf101c4c242
joshua regio <regiojosh1@gmail.com>
2022-03-26 23:50:47 +0800

    Fix error in PluggularSideNav spec

--- projects/common/src/lib/sidenav-item/sidenav-item.component.spec.ts
+++ projects/common/src/lib/sidenav-item/sidenav-item.component.spec.ts
@@ -1,20 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SidenavItemComponent } from './sidenav-item.component';
+import { PluggularSideNavItemComponent } from './sidenav-item.component';
 
 describe('SidenavItemComponent', () => {
-  let component: SidenavItemComponent;
-  let fixture: ComponentFixture<SidenavItemComponent>;
+  let component: PluggularSideNavItemComponent;
+  let fixture: ComponentFixture<PluggularSideNavItemComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SidenavItemComponent ]
-    })
-    .compileComponents();
+      declarations: [PluggularSideNavItemComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SidenavItemComponent);
+    fixture = TestBed.createComponent(PluggularSideNavItemComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
