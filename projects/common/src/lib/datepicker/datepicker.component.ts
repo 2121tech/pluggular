a18c817eab97ea8867b684a23f40f9b8b7fe753a
joshua018 <regiojosh1@gmail.com>
2022-04-06 16:00:02 +0800

    Fix linting issues

--- projects/common/src/lib/datepicker/datepicker.component.ts
+++ projects/common/src/lib/datepicker/datepicker.component.ts
@@ -7,10 +7,8 @@ import { PluggularInputComponent } from '../input/input.component';
   templateUrl: './datepicker.component.html',
   styleUrls: ['./datepicker.component.css'],
 })
-export class PluggularDatePickerComponent extends PluggularInputComponent implements OnInit {
+export class PluggularDatePickerComponent extends PluggularInputComponent {
   constructor(@Self() @Optional() public control: NgControl) {
     super(control);
   }
-
-  ngOnInit(): void {}
 }
