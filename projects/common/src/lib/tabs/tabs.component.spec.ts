19e8141c1865364f05460a92875e264fa6b946ba
joshua018 <regiojosh1@gmail.com>
2022-04-21 13:22:07 +0800

    Create test cases

--- projects/common/src/lib/tabs/tabs.component.spec.ts
+++ projects/common/src/lib/tabs/tabs.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PluggularTabsComponent } from './tabs.component';
 
@@ -15,10 +16,46 @@ describe('PluggularTabsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PluggularTabsComponent);
     component = fixture.componentInstance;
+    component.tabs = [
+      {
+        label: 'Total Compost Made',
+        tab: 'total_compost',
+      },
+      {
+        label: 'Soilmate Members',
+        tab: 'soilmate_members',
+      },
+      {
+        label: 'GHG Avoidance',
+        tab: 'ghg_avoidance',
+      },
+    ];
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the first tab as active', () => {
+    spyOn(component, 'setFirstIndexActive').and.callThrough();
+    fixture.detectChanges();
+    component.ngOnInit();
+    expect(component.setFirstIndexActive).toHaveBeenCalled();
+    expect(component.tabs[0].isActive).toBeTruthy();
+  });
+
+  it('Should call HandleTabClick when button is clicked', () => {
+    spyOn(component, 'handleTabClick').and.callThrough();
+
+    fixture.detectChanges();
+
+    const firstTab = fixture.debugElement.queryAll(By.css('li'))[0].nativeElement;
+
+    firstTab.click();
+
+    expect(component.handleTabClick).toHaveBeenCalled();
+    expect(component.tabs[0].isActive).toBeTruthy();
   });
 });
