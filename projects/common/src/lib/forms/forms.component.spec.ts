e8e6db288cc71ec46d06ba6e5759509e1cbed0f0
joshua018 <regiojosh1@gmail.com>
2022-04-21 14:03:55 +0800

    Create test cases for PluggularFormsComponent

--- projects/common/src/lib/forms/forms.component.spec.ts
+++ projects/common/src/lib/forms/forms.component.spec.ts
@@ -1,11 +1,13 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PluggularFormsComponent } from './forms.component';
 
 describe('PluggularFormsComponent', () => {
   let component: PluggularFormsComponent;
   let fixture: ComponentFixture<PluggularFormsComponent>;
-
+  let de: DebugElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PluggularFormsComponent],
@@ -15,10 +17,19 @@ describe('PluggularFormsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PluggularFormsComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call onSubmit() when form is submitted', () => {
+    spyOn(component, 'onSubmit').and.callThrough();
+    const fakeEvent = { preventDefault: () => console.log('preventDefault') };
+    fixture.debugElement.query(By.css('form')).triggerEventHandler('submit', fakeEvent);
+
+    expect(component.onSubmit).toHaveBeenCalled();
   });
 });
