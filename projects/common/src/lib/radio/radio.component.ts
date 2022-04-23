bbfcb496081c06d1b9eb447b4bac6e8f31f0c450
joshua018 <regiojosh1@gmail.com>
2022-04-23 17:31:11 +0800

    Create PluggularRadio component

---
+++ projects/common/src/lib/radio/radio.component.ts
@@ -0,0 +1,61 @@
+import { Component, Input, Optional, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+
+export type TRadioItem = {
+  label: string;
+  value: TRadioValue;
+};
+
+export type TRadioValue = string | number | boolean;
+@Component({
+  selector: 'plg-radio',
+  templateUrl: './radio.component.html',
+  styleUrls: ['./radio.component.css'],
+})
+export class PluggularRadioComponent implements ControlValueAccessor {
+  @Input() label = '';
+  @Input() items: TRadioItem[] = [];
+  @Input() required = true;
+  @Input() disabled = false;
+
+  @Input() set value(val: TRadioValue) {
+    this.writeValue(val);
+  }
+
+  get value(): TRadioValue {
+    return this._value;
+  }
+
+  private _value: TRadioValue = '';
+
+  constructor(@Self() @Optional() public control: NgControl) {
+    this.control && (this.control.valueAccessor = this);
+  }
+
+  onChange: (value: TRadioValue) => void = (): void => {
+    return;
+  };
+
+  onTouch: () => void = (): void => {
+    return;
+  };
+
+  registerOnChange(fn: () => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouch = fn;
+  }
+
+  writeValue(value: TRadioValue): void {
+    this._value = value;
+    this.onChange(value);
+  }
+
+  setValue(value: TRadioValue): void {
+    if (!this.disabled) {
+      this.writeValue(value);
+    }
+  }
+}
