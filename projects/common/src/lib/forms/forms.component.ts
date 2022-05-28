092a594f316412afe07ccc30a87b97f836b3f751
joshua018 <regiojosh1@gmail.com>
2022-05-28 20:55:30 +0800

    Form style enhancements

--- projects/common/src/lib/forms/forms.component.ts
+++ projects/common/src/lib/forms/forms.component.ts
@@ -33,6 +33,8 @@ export class PluggularFormsComponent {
   @Input() fields?: TFormFields[] = undefined;
   @Input() formGroup!: FormGroup;
   @Input() submitText? = 'Submit';
+  @Input() labelStyle? = '';
+  @Input() hideSubmitButton? = false;
 
   onSubmit(): void {
     this.hasSubmitted.emit(this.formGroup.value);
